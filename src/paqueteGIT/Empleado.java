4b2c5ade511a7d7e4070dad46adb794730c821fe
maialenperez06 <maialenp14@gmail.com>
2024-12-16 08:51:43 +0100

    Añadido constructor completo

--- src/paqueteGIT/Empleado.java
+++ src/paqueteGIT/Empleado.java
@@ -1,11 +1,23 @@
 package paqueteGIT;
 
 public class Empleado {
+	
 	private String dni;
 	private String nombre;
 	private String apellidos;
 	private byte edad;
 	private float sueldo;
 	private boolean hijos;
-
+	
+	//////CONSTRUCTORES///////////
+	public Empleado(String dni, String nombre, String apellidos, byte edad, float sueldo, boolean hijos) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+		this.sueldo = sueldo;
+		this.hijos = hijos;
+	}
+	
 }
