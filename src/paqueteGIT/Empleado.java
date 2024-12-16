0f92858e35e88dcb9b7cffd948d5bc075688b6a5
maialenperez06 <maialenp14@gmail.com>
2024-12-16 08:24:32 +0100

    commit inicial

---
+++ src/paqueteGIT/Empleado.java
@@ -0,0 +1,11 @@
+package paqueteGIT;
+
+public class Empleado {
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private byte edad;
+	private float sueldo;
+	private boolean hijos;
+
+}
